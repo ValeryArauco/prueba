baae214089bd51b4fafe89d36b937949c344139c
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-10-13 10:41:52 -0400

    Adicion funcion alta complejidad ciclomatica

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -18,3 +18,46 @@ class Calculator
     end
   end
   
+
+
+  def process_data(data)
+    result = []
+  
+    data.each do |item|
+      if item[:type] == 'A'
+        if item[:value] > 10
+          result << item[:value] * 2
+        else
+          result << item[:value] + 5
+        end
+      elsif item[:type] == 'B'
+        case item[:category]
+        when 'X'
+          result << item[:value] * 3
+        when 'Y'
+          if item[:value] % 2 == 0
+            result << item[:value] / 2
+          else
+            result << item[:value] + 10
+          end
+        else
+          result << item[:value] - 5
+        end
+      else
+        (1..item[:value]).each do |i|
+          result << i if i.even?
+        end
+      end
+    end
+  
+    if result.empty?
+      puts "No data processed"
+    elsif result.size > 20
+      puts "Too many results"
+    else
+      puts "Data processed successfully"
+    end
+  
+    result
+  end
+  
